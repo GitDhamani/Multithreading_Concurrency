22aaf197bab8a6586a87c7a2c643dbad2db9e7b0
M Dhamani <111576658+GitDhamani@users.noreply.github.com>
2022-12-23 12:49:06 +0000

    Update Program10 - Promise and Future.cpp

--- Program10 - Promise and Future.cpp
+++ Program10 - Promise and Future.cpp
@@ -3,6 +3,14 @@
 #include <mutex>
 #include <future>
 
+/*
+Program 10: Here I am making use of the Promise and Future Objects in order to extract data from a working thread 
+before it finishes. So this might be during a case where a thread has processed a value that you need in another thread. 
+In that case you pass a promise object into the thread constructor as a parameter to the function to be launched. It will 
+then modify it to fill it with a value. Back in the main thread, a Future object has been created from the Promise object 
+and can now query to see if the value has been populated, or wait until it is. This then allows transfer of information between threads.
+*/
+
 void Func(std::promise<unsigned long long>&& prom)
 {
 	unsigned long long sum;
