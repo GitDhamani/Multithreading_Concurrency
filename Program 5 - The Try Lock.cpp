35a2b985140512765aa1b4f50eead62a100b0792
M Dhamani <111576658+GitDhamani@users.noreply.github.com>
2022-12-23 12:43:51 +0000

    Update Program 5 - The Try Lock.cpp

--- Program 5 - The Try Lock.cpp
+++ Program 5 - The Try Lock.cpp
@@ -2,6 +2,13 @@
 #include <thread>
 #include <mutex>
 
+/*
+Program5: The mutex lock in the previous program is blocking, meaning that threads that are not able 
+to acquire the lock fast enough must hold in a waiting pattern until it becomes available. However there are times
+you want the thread to check if it can acquire the lock and if not, it can be free to attend to other tasks before 
+trying again later. This program shows how to achieve this using the try_lock().
+*/
+
 int count = 0;
 std::mutex m;
 
