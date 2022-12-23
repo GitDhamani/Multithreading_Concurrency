5c83cc8e624f1fb5091635f7f410d6e27c46356d
M Dhamani <111576658+GitDhamani@users.noreply.github.com>
2022-12-23 12:48:15 +0000

    Update Program 9 - Avoiding Deadlock.cpp

--- Program 9 - Avoiding Deadlock.cpp
+++ Program 9 - Avoiding Deadlock.cpp
@@ -2,6 +2,14 @@
 #include <thread>
 #include <mutex>
 
+/*
+Program 9: Here I take the deadlock from the last Program and show how to avoid it using std::lock(). 
+This is an interesting multi-mutex locking mechanism and will continuously try to lock and unlock all the mutex 
+objects you pass in to it. If successful it will lock all the mutex object parameters, otherwise it will unlock 
+any acquired mutex locks and wait on any specific one it has had trouble with. This means both threads are now 
+trying to lock the same two mutex objects at the same time, but through the use of std::lock(), there are no deadlocks.
+*/
+
 std::mutex m1, m2;
 
 void funcA()
