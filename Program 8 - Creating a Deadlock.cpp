2eeac7f05483e1a00aab5f42f0a5f763c1da1517
M Dhamani <111576658+GitDhamani@users.noreply.github.com>
2022-12-23 12:47:18 +0000

    Update Program 8 - Creating a Deadlock.cpp

--- Program 8 - Creating a Deadlock.cpp
+++ Program 8 - Creating a Deadlock.cpp
@@ -2,6 +2,14 @@
 #include <thread>
 #include <mutex>
 
+/*
+Program 8: Here I mischieviously try and create a deadlock, where the two threads get stuck waiting for each other in an 
+endless tug of war. The two threads launch FuncA and FuncB respectively. However FuncA locks mutex M1 and M2 in that order, 
+and FuncB locks mutex M2 and M1 and in that order. So what tends to happen is either function will obtain the first lock without
+any issue but will then halt whilst trying to acquire the second lock. However acquiring that lock will not be possible as the 
+other thread has already obtained it, hence the deadlock.
+*/
+
 std::mutex m1, m2;
 
 void funcA()
