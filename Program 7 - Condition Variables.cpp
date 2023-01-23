e4d0116fc9142ccb638e2c9c5817acb034e56a94
M Dhamani <111576658+GitDhamani@users.noreply.github.com>
2023-01-23 14:50:37 +0000

    Update Program 7 - Condition Variables.cpp

--- Program 7 - Condition Variables.cpp
+++ Program 7 - Condition Variables.cpp
@@ -12,13 +12,13 @@ I use this paradigm in order to ensure that the first thread is always executed 
 
 So in this case, we have started thread2 first and so in all likelyhood start2 will execute first. Which we actually don't want. 
 It will acquire the mutex lock on m1. But then cv.wait will check the status if the boolean variable correctStart which has been initialised 
-to zero. If it is zero, then it will unlock the mutex. and go to sleep because it knows it shouldn't be executing first now.
+to zero. If it is zero, then it will unlock the mutex and put its current thread to sleep because it knows it shouldn't be executing first now.
 
 So now start1 can execute, do what it needs to and then when it's finished it will release the lock, set the boolean valuable to true and 
-sent a notification to the condition variable to wake up and try again.
+sent a notification to the condition variable to wake up its thread and try again.
 
-So the conditional variable in start2 wakes up and checks the state of correctStart and this time finds it to be true. So it re-acquires 
-the lock and starts its task before releasing the lock on the way out.
+So the conditional variable in start2 wakes up its thread and checks the state of correctStart and this time finds it to be true. 
+So it re-acquires the lock and starts its task before releasing the lock on the way out.
 
 So in this way we have forced the sequence so that start1 executes first then start2.
 */
