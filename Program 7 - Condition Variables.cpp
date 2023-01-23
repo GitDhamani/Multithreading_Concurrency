ec1699e811e905139ecef883a4cb2b6977b5b346
M Dhamani <111576658+GitDhamani@users.noreply.github.com>
2023-01-23 14:52:42 +0000

    Update Program 7 - Condition Variables.cpp

--- Program 7 - Condition Variables.cpp
+++ Program 7 - Condition Variables.cpp
@@ -15,7 +15,8 @@ It will acquire the mutex lock on m1. But then cv.wait will check the status if 
 to zero. If it is zero, then it will unlock the mutex and put its current thread to sleep because it knows it shouldn't be executing first now.
 
 So now start1 can execute, do what it needs to and then when it's finished it will release the lock, set the boolean valuable to true and 
-sent a notification to the condition variable to wake up its thread and try again.
+sent a notification to the condition variable to wake up its thread and try again. Notify_one means the notification is sent out to only a
+single waiting thread, which is decided not by you but by the OS. Notify_all would have sent notifications to all waiting threads.
 
 So the conditional variable in start2 wakes up its thread and checks the state of correctStart and this time finds it to be true. 
 So it re-acquires the lock and starts its task before releasing the lock on the way out.
